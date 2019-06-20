0dd0b7305d7ac08ea0312cc02b739e188def64a3
Romain Maneschi <romain.maneschi@itk.fr>
2019-06-20 15:55:57 +0200

    :construction_worker: permit to easily dev on webcompo + add a true demo

---
+++ webcomponent/rollup.config.js
@@ -0,0 +1,25 @@
+import babel from 'rollup-plugin-babel';
+import nodeResolve from 'rollup-plugin-node-resolve';
+import path from 'path';
+
+const PACKAGE_ROOT_PATH = process.cwd();
+const PKG_JSON = require(path.join(PACKAGE_ROOT_PATH, 'package.json'));
+
+export default {
+    input: 'src/matternelle-element.js',
+    output: {
+        file: 'dist/' + PKG_JSON.name + '.js',
+        format: 'iife',
+        name: 'index',
+        sourcemap: true
+    },
+    plugins: [
+        nodeResolve({
+            jsnext: true
+        }),
+        babel({
+            exclude: 'node_modules/**',
+            extensions: ['.ts', '.js']
+        })
+    ]
+};
