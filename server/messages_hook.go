e9fc6f318f1713a28e001924a8b073f56928293f
Loic Bourgeois <loic.bourgeois@itk.fr>
2019-06-20 15:05:07 +0200

    :sparkles: Add message hook to forward the message to the app user

---
+++ server/messages_hook.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"github.com/mattermost/mattermost-server/model"
+	"github.com/mattermost/mattermost-server/plugin"
+)
+
+func (p *Plugin) MessageHasBeenPosted(c *plugin.Context, post *model.Post) {
+	if user := p.getAppUserFromPostId(post.RootId); user != nil {
+		if err := user.SendMessage(post.Message); err != nil {
+			p.API.LogError(
+				"failed to send message to app user",
+				"channel_id", post.ChannelId,
+				"user_id", post.UserId,
+				"error", err.Error(),
+			)
+		}
+	}
+}
+
+func (p *Plugin) getAppUserFromPostId(postId string) *AppUser {
+	for _, user := range p.Users {
+		if postId != "" && user.postId == postId {
+			return user
+		}
+	}
+	return nil
+}
